56e569d8b0ed352cacc189ea0bde9d5a8573806b
Edukative <56083723+Edukative@users.noreply.github.com>
2019-10-03 16:30:58 +0200

    Added Lope's second project

---
+++ Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    public float speed;
+    private float horizontalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move left/right
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        // if/else if the player goes beyond the camera's view
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }else if (transform.position.x > 10)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+    }
+}
