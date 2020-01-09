aacc8ca3c0f485bb3d9578f1494c70eb3c3faaf3
Edukative <56083723+Edukative@users.noreply.github.com>
2020-01-09 19:34:14 +0100

    Game Improved
    Spawn manager more complex

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -12,6 +12,8 @@ public class Playercontroller : MonoBehaviour
     public int health; //Player's health
     public bool isGameOver = false;
 
+    public bool restart = false;
+
     private SpriteRenderer hp1;
     private SpriteRenderer hp2;
     private SpriteRenderer hp3;
@@ -28,23 +30,35 @@ public class Playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (!isGameOver)
         {
-            Instantiate(projectile, transform.position, projectile.transform.rotation);
-        }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(projectile, transform.position, projectile.transform.rotation);
+            }
 
-        // Move left/right
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+            // Move left/right
+            horizontalInput = Input.GetAxis("Horizontal");
+            transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
-        // if/else if the player goes beyond the camera's view
-        if (transform.position.x < -xRange)
-        {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        }else if (transform.position.x > xRange)
-        {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            // if/else if the player goes beyond the camera's view
+            if (transform.position.x < -xRange)
+            {
+                transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            }
+            else if (transform.position.x > xRange)
+            {
+                transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            }
+            else if (isGameOver && Input.GetKeyDown(KeyCode.Space))
+            {
+                isGameOver = false;
+                health = 4;
+                LoseHP();
+                restart = true;
+            }
         }
+        
     }
     public void LoseHP()
     {
--- Assets/Scripts/Spawn_manager.cs
+++ Assets/Scripts/Spawn_manager.cs
@@ -14,27 +14,50 @@ public class Spawn_manager : MonoBehaviour
     //invoke values
     private float startDelay = 2.0f;
     private float spawnInterval = 1.5F;
- 
+
+    private Playercontroller playerControllerScript;
+
+    public int animalsDestroyedCount; 
+
+    public float repeatRateMin = 1;
+    public float repeatRateMax = 5;
+
+    public bool isGameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        playerControllerScript = GameObject.Find("Player").GetComponent<Playercontroller>();
+        //InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        Invoke("SpawnRandomAnimal", (Random.Range(repeatRateMin, repeatRateMax)));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerControllerScript.restart)
+        {
+            Invoke("SpawnObstacle", (Random.Range(repeatRateMin, repeatRateMax)));
+            playerControllerScript.restart = false;
+        }
     }
 
    
 
     void SpawnRandomAnimal()
     {
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-        int animalIndex = Random.Range(0, animals.Length);
-        Instantiate(animals[animalIndex], spawnPos,
-            animals[animalIndex].transform.rotation);
+        if (!isGameOver)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animals.Length);
+            GameObject animal = Instantiate(animals[animalIndex], spawnPos,
+                animals[animalIndex].transform.rotation);
+            Fly_forward animScript = animal.GetComponent<Fly_forward>();
+            animScript.speed = animScript.speed + (float)animalsDestroyedCount;
+
+            float randomDelay = Random.Range(repeatRateMin, repeatRateMax);
+            Debug.Log("Random interval" + randomDelay);
+            Invoke("SpawnRandomAnimal", randomDelay);
+        }
     }
 }
